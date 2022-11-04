18744681f82037de19a2c3db2e90e35fcd449558
Dhruv Gamdha <dgamdha@iastate.edu>
2022-11-04 02:24:07 -0500

    cleaned up the bubbleanalysis and utils file

--- bubbleAnalysis.py
+++ bubbleAnalysis.py
@@ -1,68 +1,81 @@
 # Class for bubble analysis
 import os
 class bubbleAnalysis:
-    def __init__(self, videoPath, videoName, baseResultDirPath):
+    def __init__(self, newResultsDir, videoDirPath, videoName, baseResultDirPath, resultsDir):
+        # Create a dictionary to store all the path names
+        self.DirNames = {
+            1: 'videoDirPath',
+            2: 'vidFramesDirPath',
+            3: 'baseResultDirPath',
+            4: 'resultDirPath'
+        } 
+        
         self.paths                      = {}            # Dictionary of paths to the directories and files
-        self.paths['videoDirPath']      = videoPath 
-        self.paths['vidFramesDirPath']  = os.path.join(self.paths['videoDirPath'], 'frames')
-        self.paths['baseResultDirPath'] = baseResultDirPath
+        self.paths[self.DirNames[1]]    = videoDirPath 
+        self.paths[self.DirNames[2]]    = os.path.join(self.paths[self.DirNames[1]], 'frames')
+        self.paths[self.DirNames[3]]    = baseResultDirPath
+        self.videoName                  = videoName
+        self.subResultDirNames          = ['binary', 'binary/all', 'binary/all/frames', 'analysis', 'analysis/pixSize', 'analysis/vertPos', 'analysis/dynamicMarker']
+        self.videoFPS                   = 60.0          # FPS of the video
+        self.frameNameTemplate          = 'frame%d.png' # Name template of the frames
+        self.flowType                   = 2             # 1: fluidFlow1, 2: fluidFlow2
+        self.newResultsDir              = newResultsDir
         
-        self.videoName          = videoName
-        self.resVersionDirName  = 'version'
-        self.subResultDirNames  = ['binary', 'binary/all', 'binary/all/frames', 'analysis', 'analysis/pixSize', 'analysis/vertPos', 'analysis/dynamicMarker']
-        
-        self.videoFPS           = 60.0          # FPS of the video
-        self.frameNameTemplate  = 'frame%d.png' # Name template of the frames
-        self.flowType           = 2             # 1: fluidFlow1, 2: fluidFlow2
+        if self.newResultsDir:
+            self.resultsDirTemplate  = 'version'
+        else:    
+            if not os.path.exists(os.path.join(baseResultDirPath, resultsDir)):     # Check if the results directory exists
+                exit("ERROR: Results directory does not exist")
+            else:
+                self.resultsDirTemplate  = resultsDir
         self.createResultDirs()
         
     def createResultDirs(self):
         from directories import directories
-        dirObj = directories(self.paths['baseResultDirPath'], self.resVersionDirName, self.subResultDirNames)
+        dirObj = directories(self.paths[self.DirNames[3]], self.resultsDirTemplate, self.subResultDirNames, self.newResultsDir)
         
-        self.paths['resultDirPath'] = dirObj.getVerDirPath()
+        self.paths[self.DirNames[4]] = dirObj.getVerDirPath()
         self.paths.update(dirObj.getSubDirPaths())
         
     def getFramesFromVideo(self):
         from utils import readAndSaveVid
-        videoPath = os.path.join(self.paths['videoDirPath'], self.videoName)
-        readAndSaveVid(videoPath, self.paths['vidFramesDirPath'])
+        videoPath = os.path.join(self.paths[self.DirNames[1]], self.videoName)
+        readAndSaveVid(videoPath, self.paths[self.DirNames[2]])
     
     def getBinaryImages(self):
         from utils import processImages
-        para = self.cropParameters(self.flowType)
-        processImages(self.paths['vidFramesDirPath'], self.paths['binary/all/frames'], para['top'], para['bottom'], para['left'], para['right'], para['blockSize'], para['constantSub'], para['minSize'], para['connectivity'])
+        params = self.flowTypeParams(self.flowType)
+        processImages(self.paths[self.DirNames[2]], self.paths['binary/all/frames'], params)
         
     def createVideoFromFrames(self, frameDir, videoDir):
         from utils import makeSingleVideo
         from os.path import join
-        videoPath = join(videoDir, 'video.mp4')
+        videoPath = join(self.paths[videoDir], 'video.mp4')
         makeSingleVideo(self.paths[frameDir], self.frameNameTemplate, videoPath, self.videoFPS)
           
     def createConcatVideo(self, lftFrameDir, rhtFrameDir, videoDir):
         from utils import makeConcatVideos
-        para = self.cropParameters(self.flowType)
-        videoLocAndName_mp4 = os.path.join(videoDir, 'video.mp4')
-        makeConcatVideos(self.paths[lftFrameDir], self.paths[rhtFrameDir], self.frameNameTemplate, videoLocAndName_mp4, self.videoFPS, para['top'], para['bottom'], para['left'], para['right'])    
+        params = self.flowTypeParams(self.flowType)
+        videoLocAndName_mp4 = os.path.join(self.paths[videoDir], 'video.mp4')
+        makeConcatVideos(self.paths[lftFrameDir], self.paths[rhtFrameDir], self.frameNameTemplate, videoLocAndName_mp4, self.videoFPS, params)    
     
     def binaryAnalysis(self):
         from utils import dropAnalysis
-        para = self.cropParameters(self.flowType)
-        dropAnalysis(self.paths['binary/all/frames'], self.paths['analysis'], self.frameNameTemplate, para['connectivity'])
+        params = self.flowTypeParams(self.flowType)
+        dropAnalysis(self.paths['binary/all/frames'], self.paths['analysis'], self.frameNameTemplate, params)
     
-    def cropParameters(self, flowType):
-        # Crop the frame from center corresponding to below values, original size is 400x800
+    def flowTypeParams(self, flowType):
         para = {}
         if flowType == 1:
             # fluidFlow1 crop_v1 parameters
-            para['top']     = 100
-            para['bottom']  = 700
-            para['left']    = 100
-            para['right']   = 300
-            para['blockSize']       = 9
-            para['constantSub']       = 5
+            para['top']          = 100
+            para['bottom']       = 700
+            para['left']         = 100
+            para['right']        = 300
+            para['blockSize']    = 9
+            para['constantSub']  = 5
             para['connectivity'] = 2
-            para['minSize'] = 1
+            para['minSize']      = 1
             ''' 
             # fluidFlow1 crop_v3 parameters
             para['top']     = 120
@@ -71,17 +84,17 @@ class bubbleAnalysis:
             para['right']   = 250 
             '''   
             return para
-         
+        
         elif flowType == 2:
             # fluidFlow2 crop_v1 parameters
-            para['top']     = 100
-            para['bottom']  = 750
-            para['left']    = 105
-            para['right']   = 200
-            para['blockSize']       = 41
-            para['constantSub']       = 7
+            para['top']          = 100
+            para['bottom']       = 750
+            para['left']         = 105
+            para['right']        = 200
+            para['blockSize']    = 41
+            para['constantSub']  = 7
             para['connectivity'] = 1
-            para['minSize'] = 1
+            para['minSize']      = 1
             
             return para
         
--- utils.py
+++ utils.py
@@ -47,62 +47,57 @@ def readAndSaveVid(videoFile, saveFramePath):
         th2 = cv2.adaptiveThreshold(frame,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,i,j)
         cv2.imwrite(join(binaryPath, "frames", frameName), th2)
  '''
-def processImages(framePath, binaryPath, top, bottom, left, right, blockSize, constSub, min_size, connectivity):
+def processImages(framePath, binaryPath, params):
     import cv2
     import os
     from os.path import join
     import numpy as np
     import matplotlib.pyplot as plt
     import skimage.measure as skm
-    # from skimage.morphology import remove_small_objects # Import the package for remove small objects
-    # Import package for progress bar
     from tqdm import tqdm
+    
+    top     = params["top"]
+    bottom  = params["bottom"]
+    left    = params["left"]
+    right   = params["right"]
+    blockSize   = params["blockSize"]
+    constSub    = params["constSub"]
+    min_size    = params["min_size"]
+    connectivity    = params["connectivity"]
     
     allFramesName = [f for f in os.listdir(framePath) if (os.path.isfile(join(framePath, f)) and f.endswith(".png"))]
     
-    # Change below for loop to tqdm for loop to show progress bar with file name
     for frameName in tqdm(allFramesName, desc="Processing frames"):
-        # print(frameName)
-         
-        frame = cv2.imread(join(framePath, frameName), 0)   # Read the frame as grayscale
-        
-        frame = frame[top:bottom,left:right]    # Crop the frame from certre, original size is 400x800
-        
-        ''' 
-        # NOTE: The best parameters are i=9 and j=5, gaussian adaptive thresholding
-        i = 41
-        j = 7
-        # NOTE: The best parameters are min_size=1000 and connectivity=1
-        min_size = 1
-        connectivity = 1
-        '''
-        
-        th2 = cv2.adaptiveThreshold(frame,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,blockSize,constSub)
-        invth2 = 255 - th2   # Invert the image    
+        frame           = cv2.imread(join(framePath, frameName), 0)
+        frame           = frame[top:bottom,left:right]        
+        th2             = cv2.adaptiveThreshold(frame,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,blockSize,constSub)
+        invth2          = 255 - th2    
         labelImg, count = skm.label(invth2, connectivity=connectivity, return_num=True)
         
-        for i in range(1, count+1):         # For loop through all the labels
-            numPixels = np.sum(labelImg == i)   # Get the number of pixels in each label
-            if numPixels <= min_size:        # If the number of pixels is less than min_size, set the label to 0
+        for i in range(1, count+1):
+            numPixels = np.sum(labelImg == i)
+            if numPixels <= min_size:
                 invth2[labelImg == i] = 0
                 
-        th2 = 255 - invth2   # Invert the image back to normal
-        # print('file name : ', join(binaryPath, frameName))
+        th2 = 255 - invth2
         cv2.imwrite(join(binaryPath, frameName), th2)
    
             
-def makeConcatVideos(framePath, binaryPath, nameTemplate, videoName_avi, fps, top, bottom, left, right, cropOn = True):
+def makeConcatVideos(framePath, binaryPath, nameTemplate, videoName_avi, fps, params, cropOn = True):
     import cv2
     import os
     from os.path import join
     import numpy as np
     from tqdm import tqdm
     
-    allFramesName_unorder = [f for f in os.listdir(binaryPath) if (os.path.isfile(join(binaryPath, f)) and f.endswith(".png"))]
+    top     = params["top"]
+    bottom  = params["bottom"]
+    left    = params["left"]
+    right   = params["right"]
     
-    allFramesNum = np.zeros(len(allFramesName_unorder)) # create numpy array to store the frame numbers
+    allFramesName_unorder   = [f for f in os.listdir(binaryPath) if (os.path.isfile(join(binaryPath, f)) and f.endswith(".png"))]
+    allFramesNum            = np.zeros(len(allFramesName_unorder))
     
-    # use the name template of type "<string>%d.png" to extract the frame number from each frame name
     for i in range(len(allFramesName_unorder)):
         allFramesNum[i] = int(allFramesName_unorder[i][len(nameTemplate)-6:-4])
         
@@ -112,48 +107,35 @@ def makeConcatVideos(framePath, binaryPath, nameTemplate, videoName_avi, fps, to
     tempImg2 = cv2.imread(join(framePath, nameTemplate % allFramesNum[0]), 0)
     
     if cropOn:
-        tempImg2 = tempImg2[top:bottom,left:right]    # Crop the frame from certre, original size is 400x800
+        tempImg2 = tempImg2[top:bottom,left:right]
     
     height1, width1 = tempImg1.shape
     height2, width2 = tempImg2.shape
     
-    print("temp Img shape:",width1, height1)
-    print("temp img type:", type(tempImg1))
-    
-    # find video size
-    videoWidth = width1 + width2
-    videoHeight = max(height1, height2)
+    videoWidth = width1 + width2            # width of the video
+    videoHeight = max(height1, height2)     # height of the video
     
     # vidCodec = cv2.VideoWriter_fourcc(*'XVID')
-    vidCodec = cv2.VideoWriter_fourcc(*'mp4v')
+    vidCodec = cv2.VideoWriter_fourcc(*'mp4v')  # codec for .mp4 file
     video = cv2.VideoWriter(videoName_avi,vidCodec, fps, (videoWidth, videoHeight))
     
-    # Change below for loop to tqdm for loop to show progress bar with file name
     for frameNum in tqdm(allFramesNum, desc="Making video"):
         frm_img = cv2.imread(join(framePath, nameTemplate % frameNum))
-        
         if frm_img is None:     # Check if frame is read correctly
+            exit()
+        
+        bin_img = cv2.imread(join(binaryPath, nameTemplate % frameNum))
+        if bin_img is None:    # Check if frame is read correctly
             exit()
         
         if cropOn:
             frm_img = frm_img[top:bottom,left:right, :]
         
-        bin_img = cv2.imread(join(binaryPath, nameTemplate % frameNum))
-        
-        if bin_img is None:    # Check if frame is read correctly
-            exit()
-        
-        # make bin_img height the same size as videoHeight by adding black pixels
-        bin_img = cv2.copyMakeBorder(bin_img, 0, videoHeight - height1, 0, 0, cv2.BORDER_CONSTANT, value=[0,0,0])
-        
-        # make frm_img height the same size as videoHeight by adding black pixels
-        frm_img = cv2.copyMakeBorder(frm_img, 0, videoHeight - height2, 0, 0, cv2.BORDER_CONSTANT, value=[0,0,0])
-        
-        # Concatenate the two images horizontally (i.e. side-by-side), frm_img on the left and bin_img on the right
-        concat_img = np.concatenate((frm_img, bin_img), axis=1)
-        
+        # make bin_img and frm_img height the same size as videoHeight by adding black pixels
+        bin_img     = cv2.copyMakeBorder(bin_img, 0, videoHeight - height1, 0, 0, cv2.BORDER_CONSTANT, value=[0,0,0])
+        frm_img     = cv2.copyMakeBorder(frm_img, 0, videoHeight - height2, 0, 0, cv2.BORDER_CONSTANT, value=[0,0,0])
+        concat_img  = np.concatenate((frm_img, bin_img), axis=1)     # Concatenate the two images horizontally (i.e. side-by-side)
         video.write(concat_img) # Write the frame to video file
-         
     cv2.destroyAllWindows()
     video.release() # Now the video is saved in the current directory
 
@@ -163,41 +145,33 @@ def makeSingleVideo(framePath, nameTemplate, videoPath, fps):
     import os
     from os.path import join
     import numpy as np
+    from tqdm import tqdm
     
-    allFramesName_unorder = [f for f in os.listdir(framePath) if (os.path.isfile(join(framePath, f)) and f.endswith(".png"))]
-    
-    allFramesNum = np.zeros(len(allFramesName_unorder))  # create numpy array to store the frame numbers
+    allFramesName_unorder   = [f for f in os.listdir(framePath) if (os.path.isfile(join(framePath, f)) and f.endswith(".png"))]
+    allFramesNum            = np.zeros(len(allFramesName_unorder))  # create numpy array to store the frame numbers
     
     # use the name template of type "<string>%d.png" to extract the frame number from each frame name
     for i in range(len(allFramesName_unorder)):
         allFramesNum[i] = int(allFramesName_unorder[i][len(nameTemplate)-6:-4])
         
-    allFramesNum = np.sort(allFramesNum).astype(int)    # sort the frame numbers
-    
-    tempImg = cv2.imread(join(framePath, nameTemplate % allFramesNum[0]), 0)
-    
-    height, width = tempImg.shape
-    print("temp Img shape:",width, height)
-    print("temp img type:", type(tempImg))
+    allFramesNum    = np.sort(allFramesNum).astype(int)    # sort the frame numbers
+    tempImg         = cv2.imread(join(framePath, nameTemplate % allFramesNum[0]), 0)
+    height, width   = tempImg.shape
     
     # Define the codec and create VideoWriter object
     # vidCodec = cv2.VideoWriter_fourcc(*'XVID')
-    vidCodec = cv2.VideoWriter_fourcc(*'mp4v')
-    video = cv2.VideoWriter(videoPath, vidCodec, fps, (width, height))
+    vidCodec    = cv2.VideoWriter_fourcc(*'mp4v')
+    video       = cv2.VideoWriter(videoPath, vidCodec, fps, (width, height))
     
-    for frameNum in allFramesNum:
-        print(frameNum)
+    for frameNum in tqdm(allFramesNum, desc="Making video"):
         frm_img = cv2.imread(join(framePath, nameTemplate % frameNum))
-        
         if frm_img is None:     # Check if frame is read correctly
             exit()
-        
         video.write(frm_img)    # feed the concatenated image to the video writer
-         
     cv2.destroyAllWindows()
     video.release()            # Now the video is saved in the current directory
 
-def dropAnalysis(binaryPath, analysisPath, nameTemplate, connectivity):
+def dropAnalysis(binaryPath, analysisPath, nameTemplate, params):
     import cv2
     import os
     from os.path import join
@@ -205,6 +179,7 @@ def dropAnalysis(binaryPath, analysisPath, nameTemplate, connectivity):
     import skimage.measure as skm
     import matplotlib.pyplot as plt
     
+    connectivity = params["connectivity"]
     allFrameNames_unorder = [f for f in os.listdir(binaryPath) if (os.path.isfile(join(binaryPath, f)) and f.endswith(".png"))]    # Read the binary images
     
     allFramesNum = np.zeros(len(allFrameNames_unorder))  # create numpy array to store the frame numbers
@@ -215,9 +190,9 @@ def dropAnalysis(binaryPath, analysisPath, nameTemplate, connectivity):
     
     allFramesNum = np.sort(allFramesNum).astype(int)        # sort the frame numbers
     
-    frame_bubbleCount = []              # bubble count, frame wise
-    frame_bubblePixSizes = []           # pixel sizes of all the bubbles, frame wise
-    frame_bubbleVertPos = []            # vertical position of all the bubbles, frame wise
+    frame_bubbleCount       = []              # bubble count, frame wise
+    frame_bubblePixSizes    = []           # pixel sizes of all the bubbles, frame wise
+    frame_bubbleVertPos     = []            # vertical position of all the bubbles, frame wise
     
     for frameNum in allFramesNum:
         img = cv2.imread(join(binaryPath, nameTemplate % frameNum), 0)    # read the image
@@ -226,14 +201,10 @@ def dropAnalysis(binaryPath, analysisPath, nameTemplate, connectivity):
         labelImg, count = skm.label(img, connectivity=connectivity, return_num=True)
         frame_bubbleCount.append(count)
         
-        print("Frame Number: ", frameNum, "Bubble Count: ", count)   # Print frame number and bubble count
-        
-        # Get the bubble details
         bubble_pixSize = []         # bubble pixel size, bubble wise
         bubble_vertPos = []        # bubble highest pixel vertical position, bubble wise
         
         for i in range(1,count+1):
-        # for i in range(5,6):
             rows, cols = np.where(labelImg == i)
             bubble_pixSize.append(len(rows))        # adding ith bubble pixel size
             vertPos = np.min(rows)                  # finding the highest pixel vertical position
@@ -268,7 +239,6 @@ def dropAnalysis(binaryPath, analysisPath, nameTemplate, connectivity):
         
         # scatter plot the bubble vertical position, with x axis label as the bubble number, plot title as frame number, set the x and y axis limits
         plt.scatter(range(1, count+1), bubble_vertPos, s=bubble_pixSize)
-        # plt.plot(bubble_vertPos)
         plt.xlabel("Bubble Number")
         plt.ylabel("Bubble Vertical Position")
         plt.title("bubble positions in Frame Number: " + str(frameNum))
@@ -287,67 +257,3 @@ def dropAnalysis(binaryPath, analysisPath, nameTemplate, connectivity):
     plt.close()
     
     plt.close('all')
-
-
-def roughWork():
-       
-    ''' 
-    # Show the frame in grayscale
-    cv2.imshow("Original", frame)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    '''
-    
-    ''' 
-    # Apply blurring to the frame
-    Gaussian = cv2.GaussianBlur(frame, (3, 3), 0)
-    
-    # Show the frame in grayscale
-    cv2.imshow("Gaussian", Gaussian)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    
-    # create a histogram of the blurred grayscale image
-    histogram, bin_edges = np.histogram(Gaussian, bins=256)
-
-    fig, ax = plt.subplots()
-    plt.plot(bin_edges[0:-1], histogram)
-    plt.title("Grayscale Histogram")
-    plt.xlabel("grayscale value")
-    plt.ylabel("pixels")
-    # plt.xlim(0, 1.0)
-    plt.show()
-    '''
-    
-    ''' 
-    # Apply threshold to the frame starting from 100 to 255 at 30 steps
-    # for i in range(50,255,10):
-        # ret, thresh = cv2.threshold(frame, i, 255, cv2.THRESH_BINARY)
-        # cv2.imwrite(join("thresh", "thresh%d.png" % i), thresh)
-        
-    # ret, OTSUthresh = cv2.threshold(frame, 0, 255, cv2.THRESH_BINARY + + cv2.THRESH_OTSU)
-    # cv2.imwrite(join("thresh", "OTSUthresh.png"), OTSUthresh)
-    
-    # # Otsu's thresholding after Gaussian filtering
-    # blur = cv2.GaussianBlur(frame,(5,5),0)
-    # ret3,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-    # cv2.imwrite(join("thresh", "otsu_gaussianFiltering.png"), th3)
-    
-    # # Adaptive mean thresholding
-    # th2 = cv2.adaptiveThreshold(frame,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,11,2)
-    # cv2.imwrite(join("thresh", "adaptiveMeanThresh.png"), th2)
-    '''
-    
-    # # Adaptive Gaussian thresholding
-    # th3 = cv2.adaptiveThreshold(frame,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
-    # cv2.imwrite(join("thresh", "adaptiveGaussThreshold.png"), th3)
-    
-    # Find right parameter for adaptive thresholding
-    # for i in range(3, 20, 2):
-    #     j = 2
-    #     th2 = cv2.adaptiveThreshold(frame,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,i,j)
-    #     cv2.imwrite(join("thresh", "adaptiveGaussThreshold", "agt%d_%d.png" % (i,j)), th2)
-    # for j in range(1, 20, 1):
-    #     i = 9
-    #     th2 = cv2.adaptiveThreshold(frame,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,i,j)
-    #     cv2.imwrite(join("thresh", "adaptiveGaussThreshold", "agt%d_%d.png" % (i,j)), th2)
