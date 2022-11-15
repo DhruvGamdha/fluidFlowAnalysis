f59d552117554436d77fb877251bdd3dbd52eb49
Dhruv Gamdha <dgamdha@iastate.edu>
2022-11-15 14:46:22 -0600

    updated main file

--- main.py
+++ main.py
@@ -2,17 +2,20 @@ from bubbleAnalysis import bubbleAnalysis
 
 # main function
 if __name__ == "__main__":
-    videoPath   = 'data/fluidFlow2/'
-    videoName   = 'Extrusions_5000cst_500fps_run2_drop2_cut.avi'
-    baseResDir  = 'results/fluidFlow2/'
-    newResultsDir = False
-    resultsDir = 'version_1'
+    baseVideoPath= 'data/fluidFlow2/'
+    baseResPath  = 'results/fluidFlow2/'
+    # newResultsDir = False
+    resultsDirVerNum    = 1     # Either -1 (for new results directory) or the version number
+    dataDirVerNum       = 1     # Either -1 (for new data directory) or the version number
     
-    analysis    = bubbleAnalysis(newResultsDir, videoPath, videoName, baseResDir, resultsDir)
+    analysis    = bubbleAnalysis(baseVideoPath, baseResPath, resultsDirVerNum, dataDirVerNum)
+    analysis.getFramesFromVideo()
     # analysis.getBinaryImages()
-    analysis.createVideoFromFrames('binary/all/frames', 'binary/all')
+    # analysis.createVideoFromFrames('binary/all/frames', 'binary/all')
     # analysis.binaryAnalysis()
-    # analysis.createConcatVideo('vidFramesDirPath', 'binary/all/frames', 'binary/all')
+    # analysis.createConcatVideo('binary/all/frames', 'analysis/dynamicMarker', 'analysis/dynamicMarker')
+    # analysis.createConcatVideo('binary/all/frames', 'analysis/pixSize', 'analysis/pixSize')
+    # analysis.createConcatVideo('binary/all/frames', 'analysis/vertPos', 'analysis/vertPos')
     
     print("Done")
     
