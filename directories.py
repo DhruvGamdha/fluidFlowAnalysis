2569810484dddbd934eb09c41303c198bca164e3
Dhruv Gamdha <dgamdha@iastate.edu>
2022-12-20 23:37:10 -0600

    updated directories class

--- directories.py
+++ directories.py
@@ -5,34 +5,27 @@ import re
 class directories:
     def __init__(self, baseDirPath, dirNames, versionIndex):
         self.baseDirPath    = baseDirPath
-        self.dirNames    = dirNames
-        self.dirPaths    = {}
-        self.verDirName = self.getVersionDirName(self.dirNames[0], versionIndex)
+        self.dirNames       = dirNames
+        self.dirPaths       = {}
+        self.verDirName     = self.getVersionDirName(self.dirNames[0], versionIndex)
         self.createDirs()
-        
+    
     def getVersionDirName(self, template, versionIndex):
-        
-        if versionIndex == -1:        
+        if versionIndex == -1:
             lenFolderName   = len(template)
             dirlist         = [int(item[lenFolderName:]) for item in os.listdir(self.baseDirPath) if os.path.isdir(os.path.join(self.baseDirPath,item)) and re.search(template, item) != None and len(item)> lenFolderName] 
-            
+            versionIndex    = 1
             if len(dirlist) != 0:
-                latestVersion = max(dirlist) + 1
-            else:
-                latestVersion = 1
-        else:
-            latestVersion = versionIndex
-            
-        versionDirName = template + str(latestVersion)            
+                versionIndex = max(dirlist) + 1
+        
+        latestVersion   = versionIndex
+        versionDirName  = template + str(latestVersion)            
         return versionDirName
                 
     def createDirs(self):
-        # Create version directory if it does not exist
-        self.dirPaths[self.dirNames[0]] = createDirectory(self.baseDirPath, self.verDirName)
-        
-        # Create remaining sub directories wrt version directory
-        for subDirName in self.dirNames[1:]:
-            self.dirPaths[subDirName] = createDirectory(self.dirPaths[self.dirNames[0]], subDirName)
+        self.dirPaths[self.dirNames[0]] = self.createDirectory(self.baseDirPath, self.verDirName)   # Create version directory
+        for subDirName in self.dirNames[1:]:                                                        # Create sub directories wrt version directory
+            self.dirPaths[subDirName] = self.createDirectory(self.dirPaths[self.dirNames[0]], subDirName)
             
     def getDirPaths(self):
         return self.dirPaths
@@ -40,9 +33,7 @@ class directories:
     def getDirNames(self):
         return self.dirNames
 
-def createDirectory(path, directoryName):
-        import os
-        from os.path import join
+    def createDirectory(self, path, directoryName):
         directory = join(path, directoryName)
         if not os.path.exists(directory):
             os.makedirs(directory)
